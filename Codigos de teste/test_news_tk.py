ad5c3e5c3bbea71cb1e4d006f62f31768cfcdf64
Nicolau Tahan <nicolautahan@gmail.com>
2017-03-16 11:09:30 -0300

    Adicionando comentarios

--- Codigos de teste/test_news_tk.py
+++ Codigos de teste/test_news_tk.py
@@ -1,7 +1,13 @@
+# -----------------------------------------	#
+# Teste Tkinter, Feed and News v1.0			#
+#											#
+# Nicolau Tahan - 15/03/17					#
+# -----------------------------------------	#
+
 import feedparser
 from Tkinter import *
 
-refresh_rate = 300000
+refresh_rate = 300000 # Refresh nas noticias a cada 5 min
 text_size = 25
 fonte = 'Helvetica'
 
@@ -9,24 +15,32 @@ class Noticias(Frame):
 	def __init__(self, root):
 		Frame.__init__(self, root, bg='black')
 
+		# So um titulozinho pra parte das noticias
+		# PQ Q ESSA PORRA NAO CONSEGUE FICAR NA DIREITA????
 		self.newsLB = Label(self, text = 'Noticias', font=(fonte, text_size), bg = 'black', fg = 'white')
 		self.newsLB.pack(side = TOP, anchor = E)
 
-		self.titleFR = Frame(self, bg= 'black')
+		# Precisamos declarar essa Frame para os titulos das noticias
+		self.titleFR = Frame(self, bg= 'black')	
 		self.titleFR.pack(side = TOP, anchor = W)
 
 		self.get_noticias()
 
 	def get_noticias(self):
+
+		# Noticias originam do RSS feed do Google News, valeu migos
 		self.news_url = 'http://news.google.com/news?ned=pt-BR_br&output=rss'
 		self.feed = feedparser.parse(self.news_url)
 		
+		# Usamos apenas as 3 ultimas noticias
 		for noticias in self.feed.entries[0:3]:
 			self.post = Titulo(self.titleFR, noticias.title)
 
 		self.newsLB.after(refresh_rate, self.get_noticias)
 
 
+# Mais facil criar uma classe para cada noticia pois assim as 
+# mesmas ja vem com um frame unico, diminui erros
 class Titulo(Frame):
 	def __init__(self, root, noticia = ''):
 		Frame.__init__(self, bg= 'black')
@@ -35,7 +49,8 @@ class Titulo(Frame):
 		self.noticiaLB = Label(root, text = self.titulo, font=(fonte, text_size), bg = 'black', fg = 'white')
 		self.noticiaLB.pack(side = TOP, anchor = W)
 
-			
+
+# ------------------------------------ BLOCO DE TESTE ------------------------------------------ #
 root = Tk()
 
 test_frame = Frame(root, bg = 'black')
